4cd139e945bc7b738daf45edc70298f34a5820b0
Brunno Lorran <brunnolorran.silva@gmail.com>
2024-07-07 11:43:42 -0300

    Teste de tipos primitivos de PHP

---
+++ ex003/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos de PHP</h1>
+    <?php 
+        // 0x = hexadecimal 0b = binario 0 = Octal
+        // $num = 0x1b;
+        // echo "O valor da variavel é $num";
+
+        // $valor = "Brunno Lorran";
+        // var_dump($valor);
+
+        // $num = 3e2; // 3 x 10(2) numeros com notação e
+        // echo "O valor é $num";
+
+        // $num = (int) "300"; // coerção de tipo em php
+        // var_dump($num);
+
+        // $casado = true;
+        // var_dump($casado);
+        // echo "$casado";
+        //Quando pede pra printar uma váriavel do tipo bool, se estiver false fica vazio e quando é true fica com o valor 1.
+
+        // ARRAY
+        // $vet = [3,6,"Brunno",7,2,10, false];
+        // var_dump($vet);
+
+        //OBJECT
+        // class Pessoa {
+        //     private String $nome;
+        // }
+
+        // $p = new Pessoa;
+
+        // var_dump($p);
+    ?>
+</body>
+</html>
